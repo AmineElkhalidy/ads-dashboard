da395d7c2522bea89a7513e05ed0f6bbf218be91
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-01-01 01:17:40 +0100

    Logo changed...

--- src/lib/components/Nav/Navbar.svelte
+++ src/lib/components/Nav/Navbar.svelte
@@ -52,20 +52,9 @@
     <div class="flex h-16 justify-between">
       <div class="flex">
         <div class="flex flex-shrink-0 items-center">
-          <!-- <img
-                  class="block h-8 w-auto lg:hidden"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt="Your Company"
-                />
-                <img
-                  class="hidden h-8 w-auto lg:block"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt="Your Company"
-                /> -->
-          <span class="text-xl font-semibold">FL</span>
+          <span class="text-xl font-semibold">AD</span>
         </div>
         <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-          <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
           {#each navigationLinks as link (link.href)}
             <a
               href={link.href}
